f1b0acc13d37f8d5f02e79bfc7f2407dfd11666f
Delvin Cruze <delvinc82@gmail.com>
2019-10-21 19:02:23 +0530

    Solved merge two sorted lists

---
+++ Linked list/Merge Two Sorted Lists.py
@@ -0,0 +1,64 @@
+"""
+Merge two sorted linked lists and return it as a new list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        val_list = [str(self.val)]
+        node = self.next
+        while node:
+            val_list.append(str(node.val))
+            node = node.next
+        return "->".join(val_list)
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            tail = l1
+            l1 = l1.next
+        else:
+            tail = l2
+            l2 = l2.next
+        mergedlist = tail
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return mergedlist
+
+"""
+Runtime: O(n)
+"""
+if __name__ == "__main__":
+    lol = Solution()
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    print(lol.mergeTwoLists(l1, l2))
